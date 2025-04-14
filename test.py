2dd40150c16f0da22c6f9e93bedd5627f6d0b774
dadamao <xiegodlike@gmail.com>
2025-04-14 20:15:35 +0800

    第三次修改

--- test.py
+++
@@ -1,8 +0,0 @@
-import json
-cookie = {
-    'PHPSESSID':'o3va9snhf4n95mnpr49agr1c6s',
-    '_ga':'GA1.1.955811141.1739072359',
-    '_ga_X9SNEJRE4V':'GS1.1.1739089952.2.0.1739089952.0.0.0'
-}
-d = {'a':1,'b':2}
-print(type(d))
